b1deb373bcf721432079691ee30d9a27ba10f8aa
Rohan <rohanpai126@gmail.com>
2024-08-20 16:19:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Rohan</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+     
     </div>
   );
 }
